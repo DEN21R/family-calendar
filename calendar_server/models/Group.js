7fa045bac30975287c21e131e33ae346b1901b7e
DEN21R <denr@ukr.net>
2026-05-09 22:42:02 +0300

    added model Group

---
+++ calendar_server/models/Group.js
@@ -0,0 +1,28 @@
+import mongoose from 'mongoose'
+
+const groupSchema = mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    owner: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    members: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true,
+      },
+    ],
+  },
+  { timestamps: true },
+)
+
+const Group = mongoose.model('Group', groupSchema)
+
+export default Group
