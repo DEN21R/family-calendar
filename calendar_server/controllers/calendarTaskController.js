af1376eb0448d6b869554ba79925ddabd9be58d3
DEN21R <denr@ukr.net>
2026-05-11 18:26:00 +0300

    Update calendarTaskController.js

--- calendar_server/controllers/calendarTaskController.js
+++ calendar_server/controllers/calendarTaskController.js
@@ -3,12 +3,22 @@ import CalendarTask from '../models/CalendarTask.js'
 export const createTask = async (req, res) => {
   try {
     const { title, task, date, time } = req.body
+    const { groupId } = req.params
+
+    const userId = req.user.id
 
     if (!title) {
       return res.status(400).json({ error: 'Title is required' })
     }
 
-    const newTask = await CalendarTask.create({ title, task, date, time })
+    const newTask = await CalendarTask.create({
+      title,
+      task,
+      date,
+      time,
+      groupId,
+      createdBy: userId,
+    })
     return res.status(201).json({ task: newTask })
   } catch (error) {
     return res.status(400).json({ error: error.message })
@@ -17,7 +27,9 @@ export const createTask = async (req, res) => {
 
 export const getTasks = async (req, res) => {
   try {
-    const tasks = await CalendarTask.find({})
+    const groupId = req.params.groupId
+
+    const tasks = await CalendarTask.find({ groupId })
     return res.status(200).json({ tasks })
   } catch (error) {
     return res.status(400).json({ error: 'Failed to fetch tasks' })
@@ -26,12 +38,22 @@ export const getTasks = async (req, res) => {
 
 export const updateTask = async (req, res) => {
   try {
-    const taskId = req.params.id
+    const { groupId } = req.params
+    const { id } = req.params
     const taskData = req.body
-    const task = await CalendarTask.findByIdAndUpdate(taskId, taskData, {
-      new: true,
-      runValidators: true,
-    })
+
+    const task = await CalendarTask.findOneAndUpdate(
+      { _id: id, groupId },
+      taskData,
+      {
+        new: true,
+        runValidators: true,
+      },
+    )
+
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found in this group' })
+    }
 
     return res.status(200).json({ message: 'Task updated', task })
   } catch (error) {
@@ -41,8 +63,15 @@ export const updateTask = async (req, res) => {
 
 export const deleteTask = async (req, res) => {
   try {
-    const taskId = req.params.id
-    await CalendarTask.findByIdAndDelete(taskId)
+    const { groupId } = req.params
+    const { id } = req.params
+
+    const task = await CalendarTask.findOneAndDelete({ _id: id, groupId })
+
+    if (!task) {
+      return res.status(404).json({ error: 'Task not found in this group' })
+    }
+
     return res.status(200).json({ message: 'Task deleted' })
   } catch (error) {
     return res.status(400).json({ error: 'Failed to delete task' })
