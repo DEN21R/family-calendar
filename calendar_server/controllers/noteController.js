5ee246ae1fd8ff43258a4b5332fe771873c543ab
DEN21R <denr@ukr.net>
2026-05-11 22:13:10 +0300

    Create noteController.js

---
+++ calendar_server/controllers/noteController.js
@@ -0,0 +1,73 @@
+import Note from '../models/Note.js'
+
+export const createNote = async (req, res) => {
+  try {
+    const { title, content } = req.body
+    const { groupId } = req.params
+
+    const userId = req.user.id
+
+    if (!title || !content) {
+      return res.status(400).json({ error: 'Title and content are required' })
+    }
+
+    const newNote = await Note.create({
+      title,
+      content,
+      groupId,
+      createdBy: userId,
+    })
+    return res.status(201).json({ task: newNote })
+  } catch (error) {
+    return res.status(400).json({ error: error.message })
+  }
+}
+
+export const getNote = async (req, res) => {
+  try {
+    const groupId = req.params.groupId
+
+    const notes = await Note.find({ groupId })
+    return res.status(200).json({ notes })
+  } catch (error) {
+    return res.status(400).json({ error: 'Failed to fetch notes' })
+  }
+}
+
+export const updateNote = async (req, res) => {
+  try {
+    const { groupId } = req.params
+    const { id } = req.params
+    const noteData = req.body
+
+    const note = await Note.findOneAndUpdate({ _id: id, groupId }, noteData, {
+      new: true,
+      runValidators: true,
+    })
+
+    if (!note) {
+      return res.status(404).json({ error: 'note not found in this group' })
+    }
+
+    return res.status(200).json({ message: 'note updated', note })
+  } catch (error) {
+    return res.status(400).json({ error: 'Failed to update note' })
+  }
+}
+
+export const deleteNote = async (req, res) => {
+  try {
+    const { groupId } = req.params
+    const { id } = req.params
+
+    const note = await Note.findOneAndDelete({ _id: id, groupId })
+
+    if (!note) {
+      return res.status(404).json({ error: 'note not found in this group' })
+    }
+
+    return res.status(200).json({ message: 'note deleted' })
+  } catch (error) {
+    return res.status(400).json({ error: 'Failed to delete note' })
+  }
+}
