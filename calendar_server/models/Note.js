cdfd7bb4b021905bdf1c37472f38c5f43302680a
DEN21R <denr@ukr.net>
2026-05-11 21:50:44 +0300

    Create Note.js

---
+++ calendar_server/models/Note.js
@@ -0,0 +1,32 @@
+import mongoose from 'mongoose'
+
+const noteSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    content: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    groupId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Group',
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+  },
+  { timestamps: true },
+)
+
+const Note = mongoose.model('Note', noteSchema)
+
+export default Note
