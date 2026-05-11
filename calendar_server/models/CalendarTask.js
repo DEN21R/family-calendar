f1650b7d3698a13d7af7c403463ed21a28acf6e3
DEN21R <denr@ukr.net>
2026-05-11 18:25:27 +0300

    Update CalendarTask.js

--- calendar_server/models/CalendarTask.js
+++ calendar_server/models/CalendarTask.js
@@ -1,20 +1,33 @@
 import mongoose from 'mongoose'
 
-const calendarTaskSchema = mongoose.Schema({
-  title: {
-    type: String,
-    required: true,
+const calendarTaskSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    task: {
+      type: String,
+    },
+    date: {
+      type: Date,
+    },
+    time: {
+      type: String,
+    },
+    groupId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'Group',
+      required: true,
+    },
+    createdBy: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
   },
-  task: {
-    type: String,
-  },
-  date: {
-    type: Date,
-  },
-  time: {
-    type: String,
-  },
-})
+  { timestamps: true },
+)
 
 const CalendarTask = mongoose.model('CalendarTask', calendarTaskSchema)
 
