bffc0397d2f90f65f1e4ff3a3174f19b17c5d695
DEN21R <denr@ukr.net>
2026-05-11 11:11:05 +0300

    added groupController.js and createGroup

---
+++ calendar_server/controllers/groupController.js
@@ -0,0 +1,30 @@
+import authMiddleware from '../middleware/authMiddleware.js'
+import Group from '../models/Group.js'
+
+export const createGroup = async (req, res) => {
+  try {
+    const { name } = req.body
+    const userId = req.user.id
+
+    if (!name) {
+      return res.status(400).json({ error: 'Name is required' })
+    }
+
+    const newGroup = await Group.create({
+      name,
+      owner: userId,
+      members: [userId],
+    })
+
+    return res.status(201).json({ group: newGroup })
+  } catch (error) {
+    return res.status(400).json({ error: error.message })
+  }
+}
+
+export const getMyGroups = async (req, res) => {
+  try {
+  } catch (error) {
+    return res.status(400).json({ error: error.message })
+  }
+}
