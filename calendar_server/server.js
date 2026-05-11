ca9a91676362b5dfbb69f26a893556fced1198d3
DEN21R <denr@ukr.net>
2026-05-11 22:13:27 +0300

    Update server.js

--- calendar_server/server.js
+++ calendar_server/server.js
@@ -4,6 +4,7 @@ import ConnectDB from './config/db.js'
 import calendarTaskRouter from './routes/calendarTask.js'
 import authRouter from './routes/authRoutes.js'
 import groupRouter from './routes/groupRoutes.js'
+import noteRouter from './routes/noteRoutes.js'
 import cors from 'cors'
 
 dotenv.config()
@@ -19,6 +20,7 @@ ConnectDB(mdb)
 app.use('/api/auth', authRouter)
 app.use('/api/groups', groupRouter)
 app.use('/api/groups', calendarTaskRouter)
+app.use('/api/groups', noteRouter)
 
 app.listen(port, () => {
   console.log(`Server is running on ${host}:${port}`)
