5db60ab78e8d313f4f87847a1dc1cc5be24b02e0
DEN21R <denr@ukr.net>
2026-05-14 12:19:30 +0300

    Update App.jsx

--- calendar_client/src/App.jsx
+++ calendar_client/src/App.jsx
@@ -1,13 +1,19 @@
 import './App.css'
 import { Calendar } from './pages/calendar'
 import Header from './components/header'
+import { Box } from '@mui/material'
+import { Route, Routes } from 'react-router-dom'
+import Login from './pages/auth/Login'
 
 function App() {
   return (
-    <>
+    <Box>
       <Header />
-      <Calendar />
-    </>
+      <Routes>
+        <Route path="/dashboard" element={<Login />} />
+        <Route path="/calendar" element={<Calendar />} />
+      </Routes>
+    </Box>
   )
 }
 
