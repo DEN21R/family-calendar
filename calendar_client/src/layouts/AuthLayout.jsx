65cf4a96e5e1df687fd2cfaaec6d4e680d211079
DEN21R <denr@ukr.net>
2026-05-15 17:18:31 +0300

    Create AuthLayout.jsx

---
+++ calendar_client/src/layouts/AuthLayout.jsx
@@ -0,0 +1,23 @@
+import { Box } from '@mui/material'
+import { Outlet } from 'react-router-dom'
+
+function AuthLayout() {
+  return (
+    <Box
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        px: 2,
+        py: 4,
+      }}
+    >
+      <Box sx={{ width: '100%', maxWidth: 440 }}>
+        <Outlet />
+      </Box>
+    </Box>
+  )
+}
+
+export default AuthLayout
