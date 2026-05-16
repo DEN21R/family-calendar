1fb87c2377b2a5308f93e9e8bca5b80dc5bfa534
DEN21R <denr@ukr.net>
2026-05-16 18:59:02 +0300

    Update index.jsx

--- calendar_client/src/components/footer/index.jsx
+++ calendar_client/src/components/footer/index.jsx
@@ -1,4 +1,7 @@
 import { Box, Typography } from '@mui/material'
+import XIcon from '@mui/icons-material/X'
+import FacebookIcon from '@mui/icons-material/Facebook'
+import InstagramIcon from '@mui/icons-material/Instagram'
 
 function Footer() {
   return (
@@ -7,14 +10,134 @@ function Footer() {
       sx={{
         borderTop: '1px solid rgba(32, 65, 156, 0.16)',
         py: 2,
-        px: 3,
-        textAlign: 'center',
-        backgroundColor: '#f7f9ff',
+        backgroundColor: '#e1ebfa',
       }}
     >
-      <Typography variant="body2" sx={{ color: '#20419c' }}>
-        Family Calendar
-      </Typography>
+      <Box
+        sx={{
+          display: 'grid',
+          gridTemplateColumns: { xs: '1fr 1fr', md: 'repeat(4, 1fr)' },
+          gap: 3,
+        }}
+      >
+        <Box>
+          <Typography variant="h6" sx={{ textAlign: 'center' }}>
+            Family Calendar
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              textAlign: 'center',
+              width: '100%',
+              maxWidth: 240,
+              mx: 'auto',
+              mt: 1,
+            }}
+          >
+            {' '}
+            Планируйте важное Проводите больше времени с семьей
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6" sx={{ textAlign: 'center' }}>
+            Навигация
+          </Typography>
+          <Typography variant="body2" sx={{ textAlign: 'center' }}>
+            Kалендарь
+          </Typography>
+          <Typography variant="body2" sx={{ textAlign: 'center' }}>
+            Заметки
+          </Typography>
+          <Typography variant="body2" sx={{ textAlign: 'center' }}>
+            Семья
+          </Typography>
+          <Typography variant="body2" sx={{ textAlign: 'center' }}>
+            Настройки
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6" sx={{ textAlign: 'center' }}>
+            Поддержка
+          </Typography>
+          <Typography variant="body2" sx={{ textAlign: 'center' }}>
+            Помощь
+          </Typography>
+          <Typography variant="body2" sx={{ textAlign: 'center' }}>
+            Политика конфиденциальности
+          </Typography>
+          <Typography variant="body2" sx={{ textAlign: 'center' }}>
+            Условия использования
+          </Typography>
+        </Box>
+        <Box>
+          <Typography variant="h6" sx={{ textAlign: 'center' }}>
+            Мы в соцсетях
+          </Typography>
+          <Box
+            sx={{ display: 'flex', justifyContent: 'center', gap: 2, mt: 1 }}
+          >
+            <a
+              href="https://x.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: 'inherit' }}
+            >
+              <XIcon
+                sx={{
+                  fontSize: 32,
+                  transition: '0.2s',
+                  '&:hover': { opacity: 0.6 },
+                }}
+              />
+            </a>
+
+            <a
+              href="https://facebook.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: 'inherit' }}
+            >
+              <FacebookIcon
+                sx={{
+                  fontSize: 32,
+                  transition: '0.2s',
+                  '&:hover': { opacity: 0.6 },
+                }}
+              />
+            </a>
+
+            <a
+              href="https://instagram.com"
+              target="_blank"
+              rel="noopener noreferrer"
+              style={{ color: 'inherit' }}
+            >
+              <InstagramIcon
+                sx={{
+                  fontSize: 32,
+                  transition: '0.2s',
+                  '&:hover': { opacity: 0.6 },
+                }}
+              />
+            </a>
+          </Box>
+        </Box>
+      </Box>
+      <Box
+        sx={{
+          width: '100%',
+          maxWidth: 1440,
+          mx: 'auto',
+          px: 10,
+          py: 2,
+          pt: 4,
+          textAlign: 'center',
+        }}
+      >
+        <Typography variant="body2" sx={{ color: '#20419c' }}>
+          © 2026 Family Calendar. Все права защищены.
+        </Typography>
+      </Box>
     </Box>
   )
 }
