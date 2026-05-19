404f6c07c0073f56bc9aed3ba402e19184c239f7
DEN21R <denr@ukr.net>
2026-05-19 15:59:29 +0300

    Create theme.js

---
+++ calendar_client/src/theme/theme.js
@@ -0,0 +1,17 @@
+import { createTheme } from '@mui/material/styles'
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: '#20419c',
+    },
+    secondary: {
+      main: '#f50057',
+    },
+  },
+  typography: {
+    fontFamily: '"Roboto", "Helvetica", "Arial", sans-serif',
+  },
+})
+
+export default theme
