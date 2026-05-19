9d8311272b833c3ccf1764c681e66298bd94f278
DEN21R <denr@ukr.net>
2026-05-19 16:00:47 +0300

    Update index.jsx

--- calendar_client/src/pages/calendar/index.jsx
+++ calendar_client/src/pages/calendar/index.jsx
@@ -1,10 +1,31 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 import ruLocale from '@fullcalendar/core/locales/ru'
+import { useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { Box, Button, Alert } from '@mui/material'
 
 const events = [{ title: 'Meeting', start: new Date() }]
 
 export function Calendar() {
+  const { activeGroupId } = useSelector((state) => state.group)
+  const navigate = useNavigate()
+
+  if (!activeGroupId) {
+    return (
+      <Box sx={{ py: 4 }}>
+        <Alert severity="info">Сначала выберите или создайте группу</Alert>
+        <Button
+          variant="contained"
+          onClick={() => navigate('/groups/create')}
+          sx={{ mt: 2 }}
+        >
+          Создать группу
+        </Button>
+      </Box>
+    )
+  }
+
   return (
     <div>
       <h1>Наши задания</h1>
