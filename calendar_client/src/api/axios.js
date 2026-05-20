30672b087f880f15fc398dc36baeb7d84f121ae0
DEN21R <denr@ukr.net>
2026-05-20 12:00:20 +0300

    Update axios.js

--- calendar_client/src/api/axios.js
+++ calendar_client/src/api/axios.js
@@ -20,7 +20,9 @@ apiClient.interceptors.request.use(
 apiClient.interceptors.response.use(
   (response) => response,
   (error) => {
-    if (error.response?.status === 401) {
+    const hasToken = Boolean(localStorage.getItem('token'))
+
+    if (error.response?.status === 401 && hasToken) {
       localStorage.removeItem('token')
       window.location.href = '/dashboard'
     }
