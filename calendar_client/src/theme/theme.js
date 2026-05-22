9100462e9257b1f4a1671d39a9e6f4c8e7704195
DEN21R <denr@ukr.net>
2026-05-22 11:25:07 +0300

    Update theme.js

--- calendar_client/src/theme/theme.js
+++ calendar_client/src/theme/theme.js
@@ -3,10 +3,16 @@ import { createTheme } from '@mui/material/styles'
 const theme = createTheme({
   palette: {
     primary: {
-      main: '#20419c',
+      main: '#0051f9',
     },
     secondary: {
-      main: '#f50057',
+      main: '#267dfb',
+    },
+    title: {
+      main: '#20419c',
+    },
+    logo: {
+      main: '#0a76ff',
     },
   },
   typography: {
