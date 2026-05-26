892a97edcad2049f803b9aa42c7bf7b377a64313
DEN21R <denr@ukr.net>
2026-05-26 15:54:46 +0300

    Update MainLayout.jsx

--- calendar_client/src/layouts/MainLayout.jsx
+++ calendar_client/src/layouts/MainLayout.jsx
@@ -5,12 +5,15 @@ import Footer from '../components/footer'
 
 function MainLayout() {
   return (
-    <Box sx={{ minHeight: '100vh', display: 'flex', flexDirection: 'column' }}>
+    <Box
+      sx={{
+        minHeight: '100vh',
+        display: 'flex',
+        flexDirection: 'column',
+      }}
+    >
       <Header />
-      <Box
-        component="main"
-        sx={{ flex: 1, px: { xs: 2, sm: 3, md: 4 }, py: 3 }}
-      >
+      <Box component="main" sx={{ flex: 1, py: 3 }}>
         <Outlet />
       </Box>
       <Footer />
