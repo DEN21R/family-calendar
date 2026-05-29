1badfcb81bb1fa103ae6ad7c167d0149aee96884
DEN21R <denr@ukr.net>
2026-05-29 21:22:16 +0300

    Update index.jsx

--- calendar_client/src/components/footer/index.jsx
+++ calendar_client/src/components/footer/index.jsx
@@ -7,7 +7,7 @@ import logoAl from '../../assets/logoAl.png'
 
 const Item = styled(Paper)(({ theme }) => ({
   padding: theme.spacing(2.5),
-  backgroundColor: '#c0c9e680',
+  backgroundColor: '#c4ceed80',
   display: 'flex',
   flexDirection: 'column',
   alignItems: 'flex-start',
